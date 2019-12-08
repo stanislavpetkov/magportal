a55365ec33720fbec33524a443c87c1a04dd90fc
stanislavpetkov <sunny@sunny78.net>
2019-12-08 11:02:03 +0200

    More info for restarting

--- getfb.sh
+++ getfb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sshpass -f <(printf '%s\n' 930920) ssh root@172.16.58.11 'LD_LIBRARY_PATH=/lib:/usr/local/lib:/usr/local/share/app/lib:/usr/local/n/bin /mnt/Userfs/data/fbdump -size 640,360 /mnt/Userfs/data/screen.bmp'
+sshpass -f <(printf '%s\n' 930920) ssh root@172.16.58.11 'LD_LIBRARY_PATH=/lib:/usr/local/lib:/usr/local/share/app/lib:/usr/local/n/bin /mnt/Userfs/data/fbdump -size 720,405 /mnt/Userfs/data/screen.bmp'
 sshpass -f <(printf '%s\n' 930920) scp root@172.16.58.11:/mnt/Userfs/data/screen.bmp .
